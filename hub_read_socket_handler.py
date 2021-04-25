7adf840b387275f7a56080a4695cefeb0e6a8495
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2021-04-25 14:21:00 +0100

    object oriented rsh

--- hub_read_socket_handler.py
+++ hub_read_socket_handler.py
@@ -1,43 +1,46 @@
-import socket, time, errno, pickle
-import network_config as nc
+import socket
 import bad_socket_handler as bsh
 import hub_message_handler as hmh
 import network_pickler as np
 
-def handle_read_sockets(read_sockets, server_socket, sockets_list, clients, mqtt_manager):
-    for notified_socket in read_sockets:
-        sockets_list, clients = handle_read_socket(notified_socket, server_socket, sockets_list, clients, mqtt_manager)
-        return sockets_list, clients
+class ReadSocketHandler:
+    def __init__ (self, _mqtt_manager, server_socket):
+        self.mqtt_manager = _mqtt_manager
+        self.clients = {}
+        self.server_socket = server_socket
+        self.sockets = [server_socket]
+        
+    def handle_read_sockets(self, read_sockets):
+        for notified_socket in read_sockets:
+            self.handle_read_socket(notified_socket)
+        self.clients = self.mqtt_manager.manage_queued_messages(self.clients)
 
-def handle_read_socket(notified_socket, server_socket, sockets_list, clients, mqtt_manager):
-    if notified_socket == server_socket:
-        sockets_list, clients = handle_new_connection(sockets_list, server_socket, clients)
-    else:
-        sockets_list, clients = handle_message_received(notified_socket, sockets_list, clients, mqtt_manager)
-    return sockets_list, clients
+    def handle_read_socket(self, notified_socket):
+        if notified_socket == self.server_socket:
+            self.handle_new_connection()
+        else:
+            self.handle_message_received(notified_socket, self.mqtt_manager)
 
-def handle_new_connection(sockets_list, server_socket, clients):
-    client_socket, client_address = server_socket.accept()
-    user = receive_message(client_socket)
-    if user[0] == 'OK':
-        sockets_list.append(client_socket)
-        clients[client_socket] = user[1]
-        print(f"Accepted new connection from username:{user[1]}")
-    return sockets_list, clients
+    def handle_new_connection(self):
+        client_socket, client_address = self.server_socket.accept()
+        user = self.receive_message(client_socket)
+        if user[0] == 'OK':
+            self.sockets.append(client_socket)
+            self.clients[client_socket] = user[1]
+            print(f"Accepted new connection from device: {user[1]}")
 
-def handle_message_received(notified_socket, sockets_list, clients, mqtt_manager):
-    message = receive_message(notified_socket)
-    if not message or message[0] == 'ERROR':
-        print(f"Closed connection from {clients[notified_socket]}")
-        return bsh.remove_client_socket(notified_socket, sockets_list, clients)
-    elif message[0] != 'NO_MESSAGES':
-        sender = clients[notified_socket]
-        hmh.handle_network_message(clients, notified_socket, message[1], mqtt_manager)
-    return sockets_list, clients
+    def handle_message_received(self, notified_socket, mqtt_manager):
+        message = self.receive_message(notified_socket)
+        if not message or message[0] == 'ERROR':
+            print(f"Closed connection from device: {self.clients[notified_socket]}")
+            return bsh.remove_client_socket(notified_socket, self.sockets, self.clients)
+        elif message[0] != 'NO_MESSAGES':
+            sender = self.clients[notified_socket]
+            hmh.handle_network_message(self.clients, notified_socket, message[1], mqtt_manager)
 
-def receive_message(client_socket):
-    message = np.unpickle_message(client_socket)
-    if message:
-        return message
-    else:
-        return False
+    def receive_message(self, client_socket):
+        message = np.unpickle_message(client_socket)
+        if message:
+            return message
+        else:
+            return False
--- transport_hub.py
+++ transport_hub.py
@@ -1,7 +1,7 @@
 import socket, select, queue
 import paho.mqtt.client as mqtt
 import network_config as nc
-import hub_read_socket_handler as rsh
+import hub_read_socket_handler as hrsh
 import hub_exception_socket_handler as esh
 import hub_message_handler as hmh
 import mqtt_manager as mm
@@ -10,7 +10,6 @@ socket_timeout = 1
 
 while True:
     try:
-        mqtt_manager = mm.MqttManager()
         port = nc.get_port()
         ip_address = nc.get_ip()
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,16 +17,15 @@ while True:
         server_socket.bind((ip_address, port))
         server_socket.listen()
         header_length = nc.get_header_length()
-        sockets_list = [server_socket]
-        clients = {}
+        mqtt_manager = mm.MqttManager()
+        rsh = hrsh.ReadSocketHandler(mqtt_manager, server_socket)
         while True:
-            read_sockets, write_sockets, exception_sockets = select.select(sockets_list, [], sockets_list, socket_timeout)
-            if read_sockets:
-                sockets_list, clients = rsh.handle_read_sockets(read_sockets, server_socket, sockets_list, clients, mqtt_manager)
-            if exception_sockets:
-                sockets_list, clients = esh.handle_exception_sockets(exception_sockets, sockets_list, clients)
-            clients = mqtt_manager.manage_queued_messages(clients)
+            r_socks, w_socks, e_socks = select.select(rsh.sockets, [], rsh.sockets, socket_timeout)
+            if r_socks:
+                rsh.handle_read_sockets(r_socks)
+            if e_socks:
+                esh.handle_exception_sockets(e_socks, rsh.sockets, rsh.clients)  
     except Exception as e:
-        mm.stop_client()
+        mqtt_manager.stop_client()
         print("Transport hub exception: " + str(e))
         print("Rebooting hub...")
