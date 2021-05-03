5f750eb4f2886a2ac8e87173d92db86a63a3ca61
ChrisBurrellT7145969 <t7145969@live.tees.ac.uk>
2021-05-03 19:10:59 +0100

    gps publish trigger added

--- hub_files/hub_message_handler.py
+++ hub_files/hub_message_handler.py
@@ -12,6 +12,8 @@ class HubMessageHandler():
         self.mqtt_manager.hmh = self
         self.rhm = hub_files.remote_hub_monitor.RemoteHubMonitor(self.clients, self.mqtt_manager)
         self.rsh = rsh
+        self.gps_count = 0
+        self.gps_trigger = 5
 
     def handle_network_message(self, notified_socket, message):
         try:
@@ -24,13 +26,18 @@ class HubMessageHandler():
             if sensor == 'camera_actuator' or sensor == 'microphone_actuator' or sensor == 'speaker_actuator' or sensor == 'feeder_actuator':
                 self.mqtt_manager.publish_message(sensor + self.data_string, json.dumps(message))
             elif sensor == 'gps_sensor':
-                try:
-                    gps_lat = message[0]
-                    gps_lon = message[1]
-                    self.mqtt_manager.publish_message('gps/lat', json.dumps(gps_lat))
-                    self.mqtt_manager.publish_message('gps/lng', json.dumps(gps_lon))
-                except:
-                    self.mqtt_manager.publish_message(sensor, json.dumps(message))
+                if self.gps_count == 0:
+                    try:
+                        gps_lat = message[0]
+                        gps_lon = message[1]
+                        self.mqtt_manager.publish_message('gps/lat', json.dumps(gps_lat))
+                        self.mqtt_manager.publish_message('gps/lng', json.dumps(gps_lon))
+                        
+                    except:
+                        self.mqtt_manager.publish_message(sensor, json.dumps(message))
+                self.gps_count = self.gps_count + 1
+                if self.gps_count >= self.gps_trigger:
+                    self.gps_count = 0
             else:    
                 self.mqtt_manager.publish_message(sensor, json.dumps(message))
         except Exception as e:
